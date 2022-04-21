c355e931383d074a5d9c3bf784aa05ee496d2115
Jorveth <74135308+Alex-Ukolov-1@users.noreply.github.com>
2022-04-21 14:43:29 +0300

    Update Producer.go

--- nats/Producer.go
+++ nats/Producer.go
@@ -1,17 +1,39 @@
 package main
 
-import(
+import (
+	//"flag"
 	"fmt"
-	"strconv"
-	"stan"
+	//"log"
+	//"os"
+	//"os/signal"
+	//"time"
+
+	//nats "github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+	//"github.com/nats-io/stan.go/pb"
 )
+//const DefaultNatsURL="nats://127.0.0.1:4222"
 
 func main(){
-	sc, _:=stan.Connect("prod","simple=pub")
-	defer sc.Close()
+sc, _ := stan.Connect("test-cluster", "wb-10-client")
 
-	for i:=1; ;i++{
-		sc.Publish("bestellugen", []byte("besstelung"+strconv.Itoa(i)))
-	time.Sleep(2*time.Second)
-	}
+cleanupDone := make(chan string)
+go func() {
+// Simple Synchronous Publisher
+sc.Publish("foo", []byte("soldaten!!! yaaaa")) // does not return until an ack has been received from NATS Streaming
+cleanupDone <-string("foo")
+cleanupDone<-string("soldier of fortune!") 
+}()
+fmt.Println(<-cleanupDone)
+
+// Simple Async Subscriber
+sub, _ := sc.Subscribe("foo", func(m *stan.Msg) {
+    fmt.Printf("Received a message: %s\n", string(m.Data))
+})
+
+// Unsubscribe
+sub.Unsubscribe()
+
+// Close connection
+sc.Close()
 }
